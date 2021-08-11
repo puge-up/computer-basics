adc3b3d1950e2f936a5c36a6eafe131c1d066726
puge-up <1042068690@qq.com>
2021-08-12 07:24:21 +0800

    up

---
+++ algorithm/十大排序算法/2、chooseSort.c
@@ -0,0 +1,41 @@
+/*
+选择排序，时间复杂度：O(N^2)
+*/
+
+#include<stdio.h>
+ 
+void sort(int *a, int count);
+void showArray(int *a, int count);
+ 
+void showArray(int *a, int count){
+    int i;
+ 
+    for(i = 0; i < count; i++){
+        printf("%d ", a[i]);    
+    }
+    printf("\n");
+}
+ 
+void sort(int *a, int count){
+    int i;
+    int j;
+    int tmp;
+ 
+    for(i = 0; i < count; i++){
+        for(j = i+1; j < count; j++){
+            if(a[i] > a[j]){
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+}
+ 
+void main(void){
+    int a[] = {3 ,5 ,6, 1, 7, 2, 9, 8};
+    int count = sizeof(a)/sizeof(int);
+ 
+    sort(a, count);
+    showArray(a, count);
+}
